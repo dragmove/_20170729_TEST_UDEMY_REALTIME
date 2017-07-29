7b2ac483601b3077b53c7a4d2b7dfe85c8162271
dragmove <dragmove@naver.com>
2017-07-29 23:10:25 +0900

    Task : finish 21.Observable Socket: Part 2

--- src/client/application.js
+++ src/client/application.js
@@ -1,42 +1,65 @@
-import $ from "jquery";
-import "moment-duration-format";
+import './application.scss';
 
-import "shared/operators";
+import * as services from './services';
 
-import "./application.scss";
+// playground
+services.server.on$('test')
+  .map(data => data + ' whoa')
+  .subscribe(item => {
+    console.log(`Got ${item} from server!`);
+  });
 
-import * as services from "./services";
+services.server.status$.subscribe(state => console.log(state));
 
-// ------------------
-// PLAYGROUND
-services.server
-	.emitAction$("login", {username: "foo", password: "bar"})
-	.subscribe(user => {
-		console.log("We're logged in: " + user);
-	}, error => {
-		console.error(error);
-	});
+// auth
 
-// ------------------
-// Auth
-const $html = $("html");
-services.usersStore.currentUser$.subscribe(user => {
-	if (user.isLoggedIn) {
-		$html.removeClass("not-logged-in");
-		$html.addClass("logged-in");
-	} else {
-		$html.addClass("not-logged-in");
-		$html.removeClass("logged-in");
-	}
-});
+// components
 
-// ------------------
-// Components
-require("./components/player/player");
-require("./components/users/users");
-require("./components/chat/chat");
-require("./components/playlist/playlist");
-
-// ------------------
-// Bootstrap
+// bootstrap
 services.socket.connect();
+
+
+/*
+ import $ from "jquery";
+ import "moment-duration-format";
+
+ import "shared/operators";
+
+ import "./application.scss";
+
+ import * as services from "./services";
+
+ // ------------------
+ // PLAYGROUND
+ services.server
+ .emitAction$("login", {username: "foo", password: "bar"})
+ .subscribe(user => {
+ console.log("We're logged in: " + user);
+ }, error => {
+ console.error(error);
+ });
+
+ // ------------------
+ // Auth
+ const $html = $("html");
+ services.usersStore.currentUser$.subscribe(user => {
+ if (user.isLoggedIn) {
+ $html.removeClass("not-logged-in");
+ $html.addClass("logged-in");
+ } else {
+ $html.addClass("not-logged-in");
+ $html.removeClass("logged-in");
+ }
+ });
+
+ // ------------------
+ // Components
+ require("./components/player/player");
+ require("./components/users/users");
+ require("./components/chat/chat");
+ require("./components/playlist/playlist");
+
+ // ------------------
+ // Bootstrap
+ services.socket.connect();
+ */
